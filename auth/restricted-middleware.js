bb9c99268eacc61f3ff0d09034b495db94565372
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-03-24 11:17:10 +0100

    wip

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,15 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model.js');
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = process.env.JWT_SECRET || 'silly'
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  // expects token in the Request Authorization header
+  // what value token? if no Authorization header in HTTP request?
+  const token = req.headers.authorization
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  if (token) {
+    // check whether still valid, payload not tampered with, hasn't expired etc etc
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    // if no token at all
+    res.status(400).json({ message: 'Emmm, you need a token' });
   }
-};
+}
