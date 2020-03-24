654527a8c25b238d98c1349474bfa5c59b1f7e6e
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-03-24 11:23:04 +0100

    wip

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -8,6 +8,23 @@ module.exports = (req, res, next) => {
 
   if (token) {
     // check whether still valid, payload not tampered with, hasn't expired etc etc
+    jwt.verify(
+      token,
+      JWT_SECRET,
+      (err, decodedToken) => {
+        // async code
+        if (err) {
+          // token bad
+          res.status(401).json({
+            message: 'I do not love that token, stranger'
+          })
+        } else {
+          // token good
+          req.decodedToken = decodedToken
+          next()
+        }
+      }
+    )
   } else {
     // if no token at all
     res.status(400).json({ message: 'Emmm, you need a token' });
